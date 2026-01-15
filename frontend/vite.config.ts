0065af12f7ce16db0e24880eaec0bbd6a6007899
Fabian S. <asffabian@gmail.com>
2026-01-15 13:47:59 -0800

    Allow Railway Host in vite Config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    // Aquí pegamos exactamente el dominio que te dio el error
+    allowedHosts: ['frontend-production-a0cf.up.railway.app'],
+    host: true, // Asegura que escuche en todas las IPs (0.0.0.0)
+  }
 })
